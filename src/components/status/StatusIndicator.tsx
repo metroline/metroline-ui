800d81d147d6dee8b6a981571cc0ab6b3e9c43f0
Geoffroy Empain <geoffroy@charlie-bravo.be>
2020-09-16 17:10:01 +0200

    fix(status indicator): wrong default status

--- src/components/status/StatusIndicator.tsx
+++ src/components/status/StatusIndicator.tsx
@@ -5,14 +5,14 @@ import styles from './StatusIndicator.module.scss';
 import { getStatusIcon } from './get-status-icon';
 import { PipelineJobStatus, PipelineStatus } from '../pipelines/types/pipeline';
 
-export function StatusIndicator({ status = 'unknown' }: {
+export function StatusIndicator({ status }: {
   status: PipelineStatus | PipelineJobStatus | string;
 }) {
   const { icon, spin } = getStatusIcon(status);
   return (
     <div className={classNames(styles.status, styles[`status-${status}`])}>
       {icon && (
-      <FontAwesomeIcon icon={icon} spin={spin} />
+        <FontAwesomeIcon icon={icon} spin={spin} />
       )}
     </div>
   );
