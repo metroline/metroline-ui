b4877782c63b5981e057561aa2ed57f8e2d9b309
Geoffroy Empain <geoffroy@charlie-bravo.be>
2020-10-28 22:03:20 +0100

    fix: wrong license link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,7 +12,7 @@ export function Footer() {
           <a href="https://github.com/metroline/metroline" className={styles.link} target="_blank" rel="noopener noreferrer">
             Github
           </a>
-          <a href="https://github.com/metroline/metroline/LICENSE" className={styles.link} target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/metroline/metroline/blob/master/LICENSE" className={styles.link} target="_blank" rel="noopener noreferrer">
             License
           </a>
         </div>
